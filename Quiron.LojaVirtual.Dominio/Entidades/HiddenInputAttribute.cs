1dd1d0eb61a0faf0185c90126162af5012c48015
Tiago Figueiredo <tiagostrokes@outlook.com>
2015-11-28 18:40:17 -0200

    Projeto Prointer - Tiago A. Figueiredo

---
+++ Quiron.LojaVirtual.Dominio/Entidades/HiddenInputAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    internal class HiddenInputAttribute : Attribute
+    {
+        public bool DisplayValue { get; set; }
+    }
+}
---
+++ Quiron.LojaVirtual.Web.V2/Controllers/NavController.cs
@@ -0,0 +1,67 @@
+﻿using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.V2.Models;
+
+namespace Quiron.LojaVirtual.Web.V2.Controllers
+{
+    public class NavController : Controller
+    {
+
+        private ProdutoModeloRepositorio _repositorio;
+        private ProdutosViewModel _model;
+
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine();
+            _model = new ProdutosViewModel {Produtos = produtos};
+            return View(_model);
+        }
+
+        [Route("nav/esportes/{id}/{categoria}")]
+        public ActionResult ObterProdutosPorCategoria(string id, string categoria)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine(id);
+            _model = new ProdutosViewModel { Produtos = produtos, Titulo = categoria.UpperCaseFirst() };
+            return View("Navegacao", _model);
+
+        }
+
+
+        [Route("nav/{id}/{marca}")]
+        public ActionResult ObterProdutosPorMarcas(string id, string marca)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine(marca: id);
+            _model = new ProdutosViewModel {Produtos = produtos, Titulo = marca.UpperCaseFirst()};
+            return View("Navegacao", _model);
+        }
+
+
+
+        [Route("nav/genero/{id}/{genero}")]
+        public ActionResult ObterProdutosPorGenero(string id, string genero)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine(genero: id);
+            _model = new ProdutosViewModel { Produtos = produtos, Titulo = genero.UpperCaseFirst() };
+            return View("Navegacao", _model);
+
+        }
+
+
+        [Route("nav/times/{id}/{clube}")]
+        public ActionResult ObterProdutosPorClubes(string id, string clube)
+        {
+            _repositorio = new ProdutoModeloRepositorio();
+            var produtos = _repositorio.ObterProdutosVitrine(linha: id);
+            _model = new ProdutosViewModel { Produtos = produtos, Titulo = clube.UpperCaseFirst() };
+            return View("Navegacao", _model);
+
+        }
+
+
+
+    }
+}
